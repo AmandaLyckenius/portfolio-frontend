d8165aa3fb96541a8c12bbf0c02d8a5f05449ba8
amandalyckenius <alyckenius@gmail.com>
2025-11-07 16:14:45 +0100

    feat(projects): call getProjects in ProjectSection and store result in state

--- app/components/features/projects/ProjectSection.tsx
+++ app/components/features/projects/ProjectSection.tsx
@@ -1,4 +1,31 @@
+
+import { useEffect, useState } from "react";
+import { getProjects } from "~/services/projectApi"
+import type { Project } from "~/types/api/Project"
+
+
+
 export default function ProjectSection() {
+
+    const [projects, setProjects] = useState<Project[]>([]);
+
+    useEffect (() => {
+
+        const fetchData = async () => {
+
+            const result = await getProjects();
+
+            if(result){
+                setProjects(result);
+            }
+        }
+
+        fetchData();
+        
+    }, [])
+
+
+
     return <section id="projects">
 
         Projects
