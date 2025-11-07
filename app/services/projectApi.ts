c065b8e5190c93e890e63d697cacc71752555e19
amandalyckenius <alyckenius@gmail.com>
2025-11-07 14:38:04 +0100

    feat(api): fetch projects from backend and return typed Project[]

--- app/services/projectApi.ts
+++ app/services/projectApi.ts
@@ -1,2 +1,18 @@
+import type { Project } from "~/types/api/Project";
+
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+export const getProjects = async (): Promise<Project[]> => {
+
+    const response = await fetch (`${BASE_URL}/projects`, {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json',
+        }
+
+    });
+
+    const projects = await response.json();
+
+    return projects
+}
