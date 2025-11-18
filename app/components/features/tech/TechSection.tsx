c48ae865c2cef37e3b5874d49cd411d56f3390de
amandalyckenius <alyckenius@gmail.com>
2025-11-18 14:37:41 +0100

    feat(tech): add techSection with tech stack

---
+++ app/components/features/tech/TechSection.tsx
@@ -0,0 +1,26 @@
+export default function TechSection(){
+    return (
+        <section id="tech">
+
+            <div>
+
+                <h2>Tech Stack</h2>
+
+                <div>Java</div>
+                <div>Spring Boot</div>
+                <div>React</div>
+                <div>TypeScript</div>
+                <div>PostgreSQL</div>
+                <div>MongoDB</div>
+                <div>Docker</div>
+                <div>Git / GitHub</div>
+
+
+
+
+            </div>
+
+        </section>
+
+    )
+}
