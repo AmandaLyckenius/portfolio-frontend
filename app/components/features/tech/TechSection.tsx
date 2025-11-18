f99dcdf2889476baa7277d61aa3a3bf6735fa26c
amandalyckenius <alyckenius@gmail.com>
2025-11-18 16:26:28 +0100

    refactor(tech): map tech items and extract TechBadge component

--- app/components/features/tech/TechSection.tsx
+++ app/components/features/tech/TechSection.tsx
@@ -1,3 +1,6 @@
+import TechBadge from "./TechBadge";
+import { techPositions } from "./techPositions";
+
 export default function TechSection(){
     return (
         <section id="tech" className=" min-h-screen snap-start  ">
@@ -9,30 +12,13 @@ export default function TechSection(){
                 <div>
 
                     <ul className="relative h-[500px] w-full max-w-5xl">
-                        <li className="absolute top-10 left-10">
-                            <span>Java</span>
-                        </li>
 
-                        <li className="absolute top-40 right-12">
-                            <span>Spring Boot</span>
-                        </li>
+                        {techPositions.map((tech) => (
+                            <li key={tech.name} className={`absolute ${tech.className}`}>
+                                <TechBadge>{tech.name}</TechBadge>
+                            </li>
+                        ))}
 
-                        <li className="absolute bottom-20 left-1/3">
-                            <span>React</span>
-                        </li>
-
-                        <li className="absolute top-1/2 left-20">
-                            <span>TypeScript</span>
-                        </li>
-                        <li className="absolute bottom-10 right-24">
-                            <span>PostgreSQL</span>
-                        </li>
-                        <li className="absolute bottom-28 right-1/3">
-                            <span>MongoDB</span>
-                        </li>
-                        <li className="absolute bottom-6 left-1/5">
-                            <span>Docker</span>
-                        </li>
 
                     </ul>
 
