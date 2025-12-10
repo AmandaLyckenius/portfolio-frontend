77bb0636b735ecb1881e224aec776cafd1efe637
amandalyckenius <alyckenius@gmail.com>
2025-12-10 16:08:19 +0100

    style(tech): change global colorpalette and use in techSection

--- app/components/features/tech/TechCard.tsx
+++ app/components/features/tech/TechCard.tsx
@@ -8,18 +8,18 @@ type TechCardProps = {
 
 export default function TechCard({title, icon, children}: TechCardProps){
     return (
-    <div>
+    <div className="bg-brand-surface">
 
-        <div className=" flex items-end gap-3">
+        <div className=" flex items-end gap-3 text-brand-accent">
             
 
-            <h3 className="font-semibold text-lg">{title}</h3>
+            <h3 className="font-semibold text-xl text-brand-text">{title}</h3>
 
             {icon &&
             <div>{icon}</div>}
         </div>
 
-        <p className="mt-3">{children}</p>
+        <p className="mt-3 text-base leading-relaxed text-brand-muted">{children}</p>
 
     </div>
     )
