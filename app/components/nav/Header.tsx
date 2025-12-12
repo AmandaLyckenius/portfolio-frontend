2fdebfb0489dc29a5e0ff60a63916694079a8d40
amandalyckenius <alyckenius@gmail.com>
2025-12-12 11:57:56 +0100

    style(header): make nav-bar more responsive

--- app/components/nav/Header.tsx
+++ app/components/nav/Header.tsx
@@ -2,7 +2,10 @@ export default function Header() {
     return (
     
         <nav className="fixed inset-x-0 top-6 z-50 flex justify-center">
-            <div className="flex gap-6 rounded-full border border-neutral-800 bg-neutral-900/70 px-6 py-3 shadow-lg backdrop-blur-lg text-sm text-neutral-200">
+            <div className="flex rounded-full border border-neutral-800 bg-neutral-900/70 shadow-lg backdrop-blur-lg text-sm text-neutral-200 
+            gap-3 px-4 py-4 sm:gap-4 sm:px-5 sm:py-2.5 sm:text-sm
+            md:gap-6 md:px-6 md:py-3 ">
+                
                 <a href="#hero" className="hover:text-brand-accent-soft transition-colors opacity-90 hover:opacity-100">
                     Home
                 </a>
